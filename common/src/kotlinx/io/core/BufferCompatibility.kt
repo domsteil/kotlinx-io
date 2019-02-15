22d27b8d9421a23f7526ccb7ed630c8b93c75b81
Sergey Mashkov <sergey.mashkov@jetbrains.com>
2019-02-15 10:27:20 +0300

    ~ new buffer (wip)

---
+++ common/src/kotlinx/io/core/BufferCompatibility.kt
@@ -0,0 +1,68 @@
+@file:Suppress("DeprecatedCallableAddReplaceWith")
+
+package kotlinx.io.core
+
+import kotlinx.io.bits.*
+
+
+@Deprecated("Use discard with Int parameter. No replacement")
+fun Buffer.discard(n: Long): Long = minOf(readRemaining.toLong(), n).toInt().also { discard(it) }.toLong()
+
+/**
+ * Copy available bytes to the specified [buffer] but keep them available.
+ * If the underlying implementation could trigger
+ * bytes population from the underlying source and block until any bytes available
+ *
+ * Very similar to [readAvailable] but don't discard copied bytes.
+ *
+ * @return number of bytes were copied
+ */
+fun Buffer.peekTo(buffer: Buffer): Int {
+    val size = minOf(readRemaining, buffer.writeRemaining)
+    memory.copyTo(buffer.memory, readPosition, size, buffer.writePosition)
+    discard(size)
+    buffer.commitWritten(size)
+    return size
+}
+
+/**
+ * Write byte [v] value repeated [n] times.
+ */
+fun Buffer.fill(n: Long, v: Byte) {
+    require(n >= 0)
+    require(n <= Int.MAX_VALUE)
+    memory.fill(writePosition, n.toInt(), v)
+    commitWritten(n.toInt())
+}
+
+/**
+ * Push back [n] bytes: only possible if there were at least [n] bytes read before this operation.
+ */
+@Deprecated("Use rewind instead", ReplaceWith("rewind(n)"))
+fun Buffer.pushBack(n: Int): Unit = rewind(n)
+
+@Deprecated("Use duplicate instead", ReplaceWith("duplicate()"))
+fun Buffer.makeView(): Buffer = duplicate()
+
+@Deprecated("Does nothing.")
+fun Buffer.flush() {
+}
+
+
+@Deprecated("Not supported anymore", level = DeprecationLevel.ERROR)
+fun Buffer.appendChars(csq: CharArray, start: Int, end: Int): Int = TODO()
+
+@Deprecated("Not supported anymore", level = DeprecationLevel.ERROR)
+fun Buffer.appendChars(csq: CharSequence, start: Int, end: Int): Int = TODO()
+
+@Deprecated("Not supported anymore", level = DeprecationLevel.ERROR)
+fun Buffer.append(c: Char): Appendable = TODO()
+
+@Deprecated("Not supported anymore", level = DeprecationLevel.ERROR)
+fun Buffer.append(csq: CharSequence?): Appendable = TODO()
+
+@Deprecated("Not supported anymore", level = DeprecationLevel.ERROR)
+fun Buffer.append(csq: CharSequence?, start: Int, end: Int): Appendable = TODO()
+
+@Deprecated("Not supported anymore", level = DeprecationLevel.ERROR)
+fun Buffer.append(csq: CharArray, start: Int, end: Int): Appendable = TODO()
